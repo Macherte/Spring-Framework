cc4b354316fa931657c64846e17fed778e8ced9c
kisbatyu89 <43852523+kisbatyu89@users.noreply.github.com>
2020-01-08 17:36:47 +0100

    core: added getters and setters to Building

--- core/src/main/java/RealEstate/Modules/Building.java
+++ core/src/main/java/RealEstate/Modules/Building.java
@@ -16,4 +16,108 @@ public class Building {
     private boolean gas;
     private boolean electricity;
     private boolean canal;
+
+    public BuildingType getBuildingType() {
+        return buildingType;
+    }
+
+    public void setBuildingType(BuildingType buildingType) {
+        this.buildingType = buildingType;
+    }
+
+    public double getUsefulFloorSpace() {
+        return usefulFloorSpace;
+    }
+
+    public void setUsefulFloorSpace(double usefulFloorSpace) {
+        this.usefulFloorSpace = usefulFloorSpace;
+    }
+
+    public LocalDate getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public void setYearOfConstruction(LocalDate yearOfConstruction) {
+        this.yearOfConstruction = yearOfConstruction;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getU_Value() {
+        return U_Value;
+    }
+
+    public void setU_Value(double u_Value) {
+        U_Value = u_Value;
+    }
+
+    public double getR_Value() {
+        return R_Value;
+    }
+
+    public void setR_Value(double r_Value) {
+        R_Value = r_Value;
+    }
+
+    public double getAV_Value() {
+        return AV_Value;
+    }
+
+    public void setAV_Value(double AV_Value) {
+        this.AV_Value = AV_Value;
+    }
+
+    public boolean isLowEGlazingWindows() {
+        return lowEGlazingWindows;
+    }
+
+    public void setLowEGlazingWindows(boolean lowEGlazingWindows) {
+        this.lowEGlazingWindows = lowEGlazingWindows;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    public boolean isWater() {
+        return water;
+    }
+
+    public void setWater(boolean water) {
+        this.water = water;
+    }
+
+    public boolean isGas() {
+        return gas;
+    }
+
+    public void setGas(boolean gas) {
+        this.gas = gas;
+    }
+
+    public boolean isElectricity() {
+        return electricity;
+    }
+
+    public void setElectricity(boolean electricity) {
+        this.electricity = electricity;
+    }
+
+    public boolean isCanal() {
+        return canal;
+    }
+
+    public void setCanal(boolean canal) {
+        this.canal = canal;
+    }
 }
