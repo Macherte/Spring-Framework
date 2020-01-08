7f708e5ae8b71a050dfc98386af7d9bc3b9bf1f2
kisbatyu89 <43852523+kisbatyu89@users.noreply.github.com>
2020-01-08 17:17:06 +0100

    Service: Building DAO interface created

---
+++ Service/src/main/java/dao/BuildingDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import RealEstate.Exceptions.*;
+import RealEstate.Modules.Building;
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public interface BuildingDAO {
+    void createBuilding(Building building) throws InvalidAVValueException, InvalidDateException, InvalidRValueException, InvalidUValueException;
+    Collection<Building> readAllBuildings();
+    Building readBuilding(UUID id) throws NoMatchingIdException;
+    void updateBuilding(Building building) throws InvalidAVValueException, InvalidDateException, InvalidRValueException, InvalidUValueException;;
+    void deleteBuilding(Building building);
+}
